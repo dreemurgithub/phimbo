64761cb55166f4bcfbb3924f2cd9090d34c4d700
dreemurgithub <96066405+dreemurgithub@users.noreply.github.com>
2023-05-22 22:24:48 +0700

    update display list movie dropdown by state2

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -48,7 +48,7 @@ export default function Navbar() {
                          className="bi bi-arrow-down" viewBox="0 0 16 16">
                         <path fillRule="evenodd"
                               d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-                    </svg>u
+                    </svg>
                 </div></div>
                 <div className={Styles.nav_item} onMouseEnter={()=>display_dropdown(3)} onMouseLeave={()=>display_dropdown(5)}><div >Phim Bộ
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
